47160f5018b68aa0a3004d4c5afcd0ab5a96553c
Your Name <you@example.com>
2022-11-30 14:03:26 +0100

    1.5

---
+++ 1.5/skrypt1.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('h1').on('click', function(){
+        alert('h1');
+    })
+    $('h2').on('click', function(){
+        alert('h2');
+    })
+    $('p').on('click', function(){
+        alert('p');
+    })
+    $('span').on('click', function(){
+        alert('span');
+    })
+    $('button').on('click', function(){
+        alert('button');
+    })
+})
---
+++ 1.5/skrypt2.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('h1').on('click', function(){
+        $('#wyswietl').text('h1');
+    })
+    $('h2').on('click', function(){
+        $('#wyswietl').text('h2');
+    })
+    $('p').on('click', function(){
+        $('#wyswietl').text('p');
+    })
+    $('span').on('click', function(){
+        $('#wyswietl').text('span');
+    })
+    $('button').on('click', function(){
+        $('#wyswietl').text('button');
+    })
+})
